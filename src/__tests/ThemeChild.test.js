10d5b01aebab31ac8ef47939aa9b1c24019066b3
Askadar-SCN <askadar.ru@gmail.com>
2019-10-11 15:48:48 +0300

    moving typos fix for ThemeChild.test

--- src/__tests/ThemeChild.test.js
+++ src/__tests/ThemeChild.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Child from '../Child';
+import ThemeChild from '../ThemeChild';
 // for assertions
 import { theme } from '../App'
 import { createMount } from '@material-ui/core/test-utils';
@@ -21,21 +21,21 @@ afterEach(() => {
 })
 
 it('renders without crashing', () => {
-	mount(<Provider><Child/></Provider>);
+	mount(<Provider><ThemeChild/></Provider>);
 });
 
 it('renders with proper theme values', () => {
-	const app = mount(<Provider><Child/></Provider>);
+	const app = mount(<Provider><ThemeChild/></Provider>);
 	expect(app.find('span').text()).toBe(`spacing ${theme.label.spacing}`)
 });
 
 it('renders with proper styling', () => {
-	const app = mount(<Provider><Child/></Provider>);
+	const app = mount(<Provider><ThemeChild/></Provider>);
 	// label depends on other tests for some reason, this test is 3rd in row so label is incremented to 3
 	expect(app.find('span').props().className).toBe(`makeStyles-label-3`)
 });
 
 it('extra test for prop passing', () => {
-	const app = mount(<Provider><Child testProp={37}/></Provider>);
+	const app = mount(<Provider><ThemeChild testProp={37}/></Provider>);
 	expect(app.find('Child').props().testProp).toBe(37)
 });
